0c2050b029dc4cc47636cf968ac0054bcd40b75e
nyabuganda <jayp@nyabugandas-MacBook-Pro.local>
2020-08-28 21:40:45 -0600

    created cookies

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+function attendance1(){
+    Cookies.set("first", "first1");
+    window.open("selection.html");
+
+}
+
+function attendance2(){
+    Cookies.set("second", "second2");
+    window.open("selection.html");
+
+}
+
+function attendance3(){
+    Cookies.set("third", "third3");
+    window.open("selection.html");
+
+}
---
+++ js/select.js
@@ -0,0 +1,25 @@
+
+var attendance = Cookies.get("first");
+if (attendance=="first1"){
+
+
+var first1 = document.getElementById("attendance");
+first1.innerHTML += "<p> yes I will make it! </p>";
+}
+
+
+var attendance = Cookies.get("second");
+if(attendance=="second2"){
+
+    var second = document.getElementById("attendance");
+    second.innerHTML += "<p> No, I will not be be to attend will be working! </p> ";
+
+} 
+
+var attendance = Cookies.get("third");
+if(attendance=="third3"){
+
+    var third = document.getElementById("attendance");
+    third.innerHTML += "<p> May be i will let you know you before a week </p>";
+}
+
